a1bf411d937aeff76bc61f8322d8da763fefdaab
rafacaceres93 <127420990+rafacaceres93@users.noreply.github.com>
2023-05-30 22:04:52 -0500

    ejercicio 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-function areaCirculo() {
-    const pi = 3.1416
-    const radio = parseFloat(prompt("Ingrese el radio de la circunferencia:"));
-    const area = pi * (radio * radio);
-    console.log("El área de la circunferencia es: " + area + "cm²");
-    ejercicio2Contador++;
+
+function tipoTriangulo() {
+    const lado1 = parseFloat(prompt("Ingrese el primer lado del triangulo: "));
+    const lado2 = parseFloat(prompt("Ingrese el segundo lado del triangulo: "));
+    const lado3 = parseFloat(prompt("Ingrese el tercer lado del triangulo: "));
+
+    if (lado1 === lado2 && lado2 === lado3) {
+        console.log("Es un triangulo equilatero ");
+    } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+        console.log("Es un triangulo isosceles ");
+    } else {
+        console.log("Es un triangulo escaleno ");
+    }
+    ejercicio3Contador++;
 }
