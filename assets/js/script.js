0cad222af6e916eb6f80773503f208cfd58f11ed
rafacaceres93 <127420990+rafacaceres93@users.noreply.github.com>
2023-05-30 22:10:31 -0500

    ejercicio 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 
 
-function numerosAmigos() {
-    let numero1 = parseInt(prompt("Ingrese el primer numero:"));
-    let numero2 = parseInt(prompt("Ingrese el segundo numero:"));
 
-    function obtenerSumaDivisores(numero) {
-        let suma = 0;
-        for (let i = 1; i <= numero / 2; i++) {
-            if (numero % i === 0) {
-                suma += i;
-            }
-        }
-        return suma;
+function calcularCompra() {
+    let valor = parseFloat(prompt("Ingrese el valor de compra: "));
+    let cantidad = parseInt(prompt("Ingrese la cantidad de productos: "));
+    let totalCompra = (valor * cantidad) ;
+    let descuento = totalCompra * 0.10;
+    let iva = totalCompra * 0.19;
+    let totalNeto = totalCompra + iva;
+    if (totalCompra > 1000000) {
+
+        totalNeto = totalCompra - (iva + descuento);
+      
+    } else if (totalCompra > 500000) {
+        totalNeto = totalCompra - iva;
+
     }
-
-    const sumaDivisores1 = obtenerSumaDivisores(numero1);
-    const sumaDivisores2 = obtenerSumaDivisores(numero2);
-
-    if (sumaDivisores1 === numero2 && sumaDivisores2 === numero1) {
-        console.log("Los numeros " + numero1 + " y " + numero2 + " son amigos");
-    } else {
-        console.log("Los numeros " + numero1 + " y " + numero2 + " no son amigos");
-    }
-    ejercicio4Contador++;
+    console.log("El valor de la compra es: " + totalCompra);
+    console.log("El valor a pagar es: " + totalNeto);
+    console.log("El valor de el IVA es: " + iva);
+    ejercicio5Contador++;
 }
